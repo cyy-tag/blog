fce5a68a186c5846f884fbe6d4fb0b8d2c5f3b97
cyy <12394364+cyy233@user.noreply.gitee.com>
2024-12-29 11:59:18 +0800

    更新首页样式

--- docs/.vitepress/components/PostList.vue
+++ docs/.vitepress/components/PostList.vue
@@ -5,17 +5,36 @@ import { data as posts } from '../utils/posts.data.ts'
 
 <template>
   <h1 class="recent_post">Recent Posts</h1>
+  <br><div id="test"></div><br>
   <ul class="post_list">
     <li v-for="post of posts">
       <date>{{ post.frontmatter.date }}</date><br>
       <a :href="post.url" class="post_url">{{ post.frontmatter.title }}</a>
+      <p class="description">{{ post.frontmatter.description }}</p>
     </li>
   </ul>
 </template>
 
 <style scoped>
+/* 去掉有序列表和无序列表的默认样式 */
+.post_list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-right: 300px;
+}
+
+#test {
+  width: 600px;
+  height: 2px;
+  /* background: linear-gradient(to left, #fff 10%, gainsboro 80%, #fff 85%); */
+  background-color: gray;
+  margin: auto;
+}
+
 .recent_post {
-  
+  margin-top: 100px;
+  text-align: center;
 }
 
 .post_url {
@@ -23,8 +42,12 @@ import { data as posts } from '../utils/posts.data.ts'
   text-decoration: none;
 }
 
-.post-url:hover {
+.post_url:hover {
   text-decoration: underline;
+}
+
+.description {
+  margin-top: 0%;
 }
 
 </style>
--- run_dev.sh
+++ run_dev.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #wsl2
 export CHOKIDAR_USEPOLLING=true
+npm run dev
